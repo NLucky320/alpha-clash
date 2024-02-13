7627d1d4ee951e21a8a5e5f42b9e3fd1db1438de
Nlucky320 <luckyes144@gmail.com>
2024-02-13 20:56:37 +0600

    play button js initialization done

---
+++ script.js
@@ -0,0 +1,12 @@
+// function play() {
+//   const homeSection = document.getElementById("home");
+//   homeSection.classList.add("hidden");
+
+//   const playGround = document.getElementById("play-ground");
+//   playGround.classList.remove("hidden");
+// }
+
+function play() {
+  hideElementById("home");
+  showElementById("play-ground");
+}
